e2f7ad520440e11a050e375de9437421f2cc4fc3
ady533 <65229304+ady533@users.noreply.github.com>
2024-09-22 13:28:06 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then(function(clientList) {
+      for (const client of clientList) {
+        if (client.url && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow)
+        return clients.openWindow('/');
+    })
+  );
+});
